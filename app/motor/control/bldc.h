378571be4de1af265da783b23b6bcb269c0633a1
Dmitrii Leonov <dleonov@whoosh.bike>
2026-03-21 21:06:43 +0300

    rm motor & debug files

--- app/motor/control/bldc.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BLDC_H
-#define __BLDC_H
-
-#include "../motor.h"
-#include "main.h"
-
-void BLDC_Control(u32 halls, bool direction, bool isStop);
-
-#endif /* __BLDC_H */
--- app/motor/filter.c
+++
@@ -1,5 +0,0 @@
-#include "filter.h"
-
-void TFilter_Calc(TFilter* pFilter) {
-	pFilter->output += pFilter->T * (pFilter->input - pFilter->output);
-}
