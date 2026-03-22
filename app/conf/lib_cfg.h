22f6a47bd44c473c59cfa6fe65b15fca4d07bbe0
Dmitrii Leonov <dleonov@whoosh.bike>
2026-03-23 01:45:53 +0300

    added a switch between on & off shared mutex in ring list

--- app/conf/lib_cfg.h
+++ app/conf/lib_cfg.h
@@ -3,15 +3,18 @@
 
 #include "dbg_cfg.h"
 
-#define LIB_USE_RAND 1
-
-#if LIB_USE_RAND
-#define LIB_USE_SHARED_MUTEX 1
-
-#ifdef LIB_USE_SHARED_MUTEX
 #define LIB_USE_RING_BUFF 1
 #define LIB_USE_RING_LIST 1
+// #define LIB_USE_RING_LIST_WITH_SHARED_MUTEX 1
+// #define LIB_USE_RAND	  1
+// #define LIB_USE_SHARED_MUTEX 1
+
+#ifdef LIB_USE_RING_LIST_WITH_SHARED_MUTEX
+#define LIB_USE_SHARED_MUTEX 1
+#endif /* LIB_USE_RING_LIST_WITH_SHARED_MUTEX */
+
+#if LIB_USE_SHARED_MUTEX
+#define LIB_USE_RAND 1
 #endif /* LIB_USE_SHARED_MUTEX */
-#endif /* LIB_USE_RAND */
 
 #endif /* __LIB_CFG_H */
--- lib/ring_list/ring_list.h
+++ lib/ring_list/ring_list.h
@@ -1,14 +1,18 @@
 #ifndef __RINGLIST_H
 #define __RINGLIST_H
 
+#include "def_sys.h"
+#include "def_types.h"
 #include "lib_cfg.h"
+#ifdef LIB_USE_RING_LIST_WITH_SHARED_MUTEX
 #include "shared_mutex.h"
+#endif /* LIB_USE_RING_LIST_WITH_SHARED_MUTEX */
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-#if LIB_USE_RING_LIST
+#if LIB_USE_RING_LIST || LIB_USE_RING_LIST_WITH_SHARED_MUTEX
 
 #define RING_LIST_MIN_ELEMENTS_NUM 2
 #define RING_LIST_AVOID_MUTEX	   0
@@ -20,7 +24,9 @@ typedef struct {
 	u16 CellSize;
 	u16 Capacity;
 	u32 InsertCnt;
+#ifdef LIB_USE_RING_LIST_WITH_SHARED_MUTEX
 	SharedMutex_t SharedMutex;
+#endif /* LIB_USE_RING_LIST_WITH_SHARED_MUTEX */
 } RingList_t;
 
 RET_STATE_t RingList_Init(RingList_t* pRingList, void* pLineList, u16 cellSize, u16 listSize);
@@ -33,12 +39,17 @@ u16 RingList_GetExistingElementsNum(RingList_t* pRingList);
 u16 RingList_GetLatestRingIdx(RingList_t* pRingList);
 bool RingList_IsFull(RingList_t* pRingList);
 
+#ifdef LIB_USE_RING_LIST_WITH_SHARED_MUTEX
 RET_STATE_t RingList_Flush(RingList_t* pRingList, u32 lockKey);
 RET_STATE_t RingList_InsertCell(RingList_t* pRingList, void* pCell, u32 lockKey);
+#else
+RET_STATE_t RingList_Flush(RingList_t* pRingList);
+RET_STATE_t RingList_InsertCell(RingList_t* pRingList, void* pCell);
+#endif /* LIB_USE_RING_LIST_WITH_SHARED_MUTEX */
 RET_STATE_t RingList_PeekCell(RingList_t* pRingList, void* pCell, u16 ringIdx);
 RET_STATE_t RingList_CopyToLineBuff(RingList_t* pRingList, void* pLineList, u16 listSize);
 
-#endif /* LIB_USE_RING_LIST */
+#endif /* LIB_USE_RING_LIST || LIB_USE_RING_LIST_WITH_SHARED_MUTEX */
 
 #ifdef __cplusplus
 }
