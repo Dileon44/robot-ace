8dfb4ef0b448fa14db50a3d9ad06638355bb93dd
Dmitrii Leonov <dleonov@whoosh.bike>
2026-03-29 22:11:06 +0300

    added builds for boot & correct conf files

--- boot/conf/lib_cfg.h
+++ boot/conf/lib_cfg.h
@@ -1,11 +1,11 @@
 #ifndef __LIB_CFG_H
 #define __LIB_CFG_H
 
-#define LIB_USE_SHARED_MUTEX 1
+#include "dbg_cfg.h"
 
-#ifdef LIB_USE_SHARED_MUTEX
-#define LIB_USE_RING_BUFF 1
-#define LIB_USE_RING_LIST 1
-#endif /* LIB_USE_SHARED_MUTEX */
+// #define LIB_USE_RING_BUFF    1
+// #define LIB_USE_RING_LIST    1
+// #define LIB_USE_RAND         1
+// #define LIB_USE_SHARED_MUTEX 1
 
 #endif /* __LIB_CFG_H */
