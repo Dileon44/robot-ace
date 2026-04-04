a4180858c42270cda6113fbb5d57ebb62692efe8
Dmitrii Leonov <dleonov@whoosh.bike>
2026-04-05 00:35:43 +0300

    rm unused NVIC & gpio

--- platform/m0/platform.c
+++ platform/m0/platform.c
@@ -129,8 +129,7 @@ void Pl_IWDG_ReloadCounter(void) {
 
 void Pl_USART_Debug_Init(u8* pTxBuff, u16 TxBuffLen, Pl_USART_ClbkTx_t pTxClbk, u8* pRxBuff,
 						 u16 RxBuffLen, Pl_USART_ClbkRx_t pRxClbk) {
-	GPIO_USART_Debug_Tx_Init();
-	GPIO_USART_Debug_Rx_Init();
+	GPIO_Debug_Init();
 
 	USART_Debug_Init(pRxClbk);
 	USART_Debug_Irq_Enable();
@@ -140,227 +139,61 @@ void Pl_USART_Debug_Init(u8* pTxBuff, u16 TxBuffLen, Pl_USART_ClbkTx_t pTxClbk, 
 	DMA_Debug_Irq_Enable();
 }
 
-__INLINE RET_STATE_t Pl_USART_Debug_TxData(u8* pBuff, u16 size) {
+RET_STATE_t Pl_USART_Debug_TxData(u8* pBuff, u16 size) {
 	// SYS_CRITICAL_ON();
 	RET_STATE_t retState = USART_Debug_TxData(pBuff, size, PL_USART_DEF_TIMEOUT);
 	// SYS_CRITICAL_OFF();
 	return retState;
 }
 
-__INLINE void Pl_USART_Debug_Enable_Tx(void) {
+void Pl_USART_Debug_Enable_Tx(void) {
 	DMA_Debug_Enable_Tx();
 }
 
-__INLINE void Pl_USART_Debug_Enable_Rx(void) {
+void Pl_USART_Debug_Enable_Rx(void) {
 	DMA_Debug_Enable_Rx();
 }
 
-__INLINE void Pl_USART_Debug_Disable_Tx(void) {
+void Pl_USART_Debug_Disable_Tx(void) {
 	DMA_Debug_Disable_Tx();
 }
 
-__INLINE void Pl_USART_Debug_Disable_Rx(void) {
+void Pl_USART_Debug_Disable_Rx(void) {
 	DMA_Debug_Disable_Rx();
 }
 
-__INLINE void Pl_USART_Debug_SetDataLengthRx(u32 NbData) {
+void Pl_USART_Debug_SetDataLengthRx(u32 NbData) {
 	DMA_Debug_SetDataLengthRx(NbData);
 }
 
-__INLINE void Pl_USART_Debug_SetDataLengthTx(u32 NbData) {
+void Pl_USART_Debug_SetDataLengthTx(u32 NbData) {
 	DMA_Debug_SetDataLengthTx(NbData);
 }
 
-__INLINE u32 Pl_USART_Debug_GetDataLengthTx(void) {
+u32 Pl_USART_Debug_GetDataLengthTx(void) {
 	return DMA_Debug_GetDataLengthTx();
 }
 
-__INLINE u32 Pl_USART_Debug_GetDataLengthRx(void) {
+u32 Pl_USART_Debug_GetDataLengthRx(void) {
 	return DMA_Debug_GetDataLengthRx();
 }
 
-__INLINE u8 Pl_USART_Debug_GetRxByte(void) {
+u8 Pl_USART_Debug_GetRxByte(void) {
 	return USART_Debug_GetRxByte();
 }
 
-/*========================= Platform Motor functions =========================*/
-
-u32 Pl_Motor_GetStateHalls(void) {
-	return GPIO_Motor_GetStateHalls();
+void Pl_Led_Init(void) {
+	GPIO_Led_Init();
 }
 
-void Pl_Motor_SetKeyAL(void) {
-	GPIO_Motor_SetKeyAL(GPIO_SET);
-}
-
-void Pl_Motor_SetKeyBL(void) {
-	GPIO_Motor_SetKeyBL(GPIO_SET);
-}
-
-void Pl_Motor_SetKeyCL(void) {
-	GPIO_Motor_SetKeyCL(GPIO_SET);
-}
-
-void Pl_Motor_ResetKeyAL(void) {
-	GPIO_Motor_SetKeyAL(GPIO_RST);
-}
-
-void Pl_Motor_ResetKeyBL(void) {
-	GPIO_Motor_SetKeyBL(GPIO_RST);
-}
-
-void Pl_Motor_ResetKeyCL(void) {
-	GPIO_Motor_SetKeyCL(GPIO_RST);
-}
-
-void Pl_Motor_ControlGpioInit(void) {
-	GPIO_Motor_AH_Init();
-	GPIO_Motor_BH_Init();
-	GPIO_Motor_CH_Init();
-	GPIO_Motor_AL_Init();
-	GPIO_Motor_BL_Init();
-	GPIO_Motor_CL_Init();
-}
-
-void Pl_Motor_SensorsInit(Pl_Motor_ClbkHall_t pHallClbk) {
-	GPIO_Motor_Hall_Init(pHallClbk);
-	GPIO_Motor_HallIrqEnable();
-}
-
-void Pl_Motor_PWMInit(Pl_TIM_PWM_Clbk_t pTimPwmCntTopClbk, Pl_TIM_PWM_Clbk_t pTimPwmCntBottomClbk) {
-	TIM_PWM_Init(pTimPwmCntTopClbk, pTimPwmCntBottomClbk);
-	TIM_PWM_Irq_Enable();
-	TIM_PWM_Enable();
-}
-
-void Pl_Motor_Init(Pl_TIM_PWM_Clbk_t pTimPwmCntTopClbk, Pl_TIM_PWM_Clbk_t pTimPwmCntBottomClbk,
-				   Pl_TIM_Motor_SpeedControlClbk_t pSpeedControlClbk,
-				   Pl_ADC_Motor_SensHighFreqClbk_t pAdcHighFreqClbk) {
-	Pl_Motor_ControlGpioInit();
-	Pl_Motor_PWMInit(pTimPwmCntTopClbk, pTimPwmCntBottomClbk);
-	// Pl_Motor_SensorsInit(pHallClbk);
-	Pl_TIM_SpeedControl_Init(pSpeedControlClbk);
-	Pl_ADC_HighFreqSens_Init(pAdcHighFreqClbk);
-}
-
-__INLINE void Pl_Motor_PWMA_Enable(void) {
-	TIM_PWMA_Enable();
-}
-
-__INLINE void Pl_Motor_PWMA_Disable(void) {
-	TIM_PWMA_Disable();
-}
-
-__INLINE void Pl_Motor_PWMB_Enable(void) {
-	TIM_PWMB_Enable();
-}
-
-__INLINE void Pl_Motor_PWMB_Disable(void) {
-	TIM_PWMB_Disable();
-}
-
-__INLINE void Pl_Motor_PWMC_Enable(void) {
-	TIM_PWMC_Enable();
-}
-
-__INLINE void Pl_Motor_PWMC_Disable(void) {
-	TIM_PWMC_Disable();
-}
-
-__INLINE void Pl_Motor_SetComparePWMA(u32 compareValue) {
-	TIM_PWMA_SetCompare(compareValue);
-}
-
-__INLINE void Pl_Motor_SetComparePWMB(u32 compareValue) {
-	TIM_PWMB_SetCompare(compareValue);
-}
-
-__INLINE void Pl_Motor_SetComparePWMC(u32 compareValue) {
-	TIM_PWMC_SetCompare(compareValue);
-}
-
-__INLINE u32 Pl_Motor_GetComparePWM(void) {
-	return TIM_PWM_GetCompare();
-}
-
-/*======================== Platform Sensors functions ========================*/
-
-void Pl_ADC_LowFreqSens_Init(Pl_Sensors_ADCLowFreqClbk_t pADCLowFreqClbk) {
-	TIM_ADCLowFreq_Init();
-	TIM_ADCLowFreq_Enable();
-	GPIO_ADCLowFreq_Init();
-	DMA_ADCLowFreq_Init((void*)ADC_GetLowFreqAdc(), pADCLowFreqClbk);
-	DMA_ADCLowFreq_Irq_Enable();
-	ADC_LowFreq_Init();
-}
-
-void Pl_ADC_HighFreqSens_Init(Pl_ADC_Motor_SensHighFreqClbk_t pAdcHighFreqClbk) {
-	GPIO_ADCHighFreq_Init();
-	ADC_HighFreq_Init(pAdcHighFreqClbk);
-	DMA_AdcHighFreq_Init((void*)ADC_GetHighFreqAdc(), pAdcHighFreqClbk);
-	DMA_AdcHighFreq_Irq_Enable();
-}
-
-__INLINE u32 Pl_TIM_SpeedControl_GetPrescaler(void) {
-	return TIM_SpeedControl_GetPrescaler();
-}
-
-__INLINE u32 Pl_TIM_SpeedControl_GetAutoReload(void) {
-	return TIM_SpeedControl_GetAutoReload();
-}
-
-void Pl_TIM_HallToggleTimeCalc_Init(Pl_Motor_HallToggleTimeCalcClbk_t pHallToggleTimeCalcClbk) {
-	TIM_HallToggleTimeCalc_Init(pHallToggleTimeCalcClbk);
-	TIM_HallToggleTimeCalc_IrqEnable();
-	TIM_HallToggleTimeCalc_Enable();
-}
-
-__INLINE void Pl_TIM_HallToggleTimeCalc_Enable(void) {
-	TIM_HallToggleTimeCalc_Enable();
-}
-
-__INLINE void Pl_TIM_HallToggleTimeCalc_Disable(void) {
-	TIM_HallToggleTimeCalc_Disable();
-}
-
-__INLINE void Pl_TIM_HallToggleTimeCalc_ResetCnt(void) {
-	TIM_HallToggleTimeCalc_SetCnt(0);
-}
-
-u32 Pl_TIM_HallToggleTimeCalc_GetCnt(void) {
-	return TIM_HallToggleTimeCalc_GetCnt();
-}
-
-void Pl_TIM_SpeedControl_Init(Pl_TIM_Motor_SpeedControlClbk_t pSpeedControlClbk) {
-	TIM_SpeedControl_Init(pSpeedControlClbk);
-	TIM_SpeedControl_Irq_Enable();
-	TIM_SpeedControl_Enable();
-}
-
-void Pl_DMA_AdcHighFreq_Enable(void) {
-	DMA_AdcHighFreq_Enable();
-}
-
-__INLINE void Pl_ADC_StartHighFreqConv(void) {
-	ADC_StartHighFreqConv();
-}
-
-__INLINE void Pl_LedDebug_Init(void) {
-	GPIO_LedDebug_Init();
-}
-
-__INLINE void Pl_Led_Set(void) {
+void Pl_Led_Set(void) {
 	GPIO_Led_Set();
 }
 
-__INLINE void Pl_Led_Reset(void) {
+void Pl_Led_Reset(void) {
 	GPIO_Led_Reset();
 }
 
-__INLINE void Pl_Led_Toggle(void) {
+void Pl_Led_Toggle(void) {
 	GPIO_Led_Toggle();
-}
-
-void Pl_Hall1_CallIrq(void) {
-	GPIO_Hall1_CallIrq();
 }
--- platform/platform.h
+++ platform/platform.h
@@ -106,54 +106,9 @@ u32 Pl_USART_Debug_GetDataLengthRx(void);
 u8 Pl_USART_Debug_GetRxByte(void);
 RET_STATE_t Pl_USART_Debug_TxData(u8* pBuff, u16 size);
 
-u32 Pl_Motor_GetStateHalls(void);
-void Pl_Motor_SetKeyAL(void);
-void Pl_Motor_ResetKeyAL(void);
-void Pl_Motor_SetKeyBL(void);
-void Pl_Motor_ResetKeyBL(void);
-void Pl_Motor_SetKeyCL(void);
-void Pl_Motor_ResetKeyCL(void);
-
-void Pl_Motor_SensorsInit(Pl_Motor_ClbkHall_t pHallClbk);
-void Pl_Motor_PWMInit(Pl_TIM_PWM_Clbk_t pTimPwmCntTopClbk, Pl_TIM_PWM_Clbk_t pTimPwmCntBottomClbk);
-void Pl_Motor_ControlGpioInit(void);
-void Pl_Motor_Init(Pl_TIM_PWM_Clbk_t pTimPwmCntTopClbk, Pl_TIM_PWM_Clbk_t pTimPwmCntBottomClbk,
-				   Pl_TIM_Motor_SpeedControlClbk_t pSpeedControlClbk,
-				   Pl_ADC_Motor_SensHighFreqClbk_t pAdcHighFreqClbk);
-
-void Pl_Motor_PWMA_Enable(void);
-void Pl_Motor_PWMA_Disable(void);
-void Pl_Motor_PWMB_Enable(void);
-void Pl_Motor_PWMB_Disable(void);
-void Pl_Motor_PWMC_Enable(void);
-void Pl_Motor_PWMC_Disable(void);
-void Pl_Motor_SetComparePWMA(u32 compareValue);
-void Pl_Motor_SetComparePWMB(u32 compareValue);
-void Pl_Motor_SetComparePWMC(u32 compareValue);
-u32 Pl_Motor_GetComparePWM(void);
-
-void Pl_ADC_LowFreqSens_Init(Pl_Sensors_ADCLowFreqClbk_t pADCLowFreqClbk);
-void Pl_ADC_HighFreqSens_Init(Pl_ADC_Motor_SensHighFreqClbk_t pAdcHighFreqClbk);
-
-u32 Pl_TIM_SpeedControl_GetPrescaler(void);
-u32 Pl_TIM_SpeedControl_GetAutoReload(void);
-
-void Pl_TIM_HallToggleTimeCalc_Init(Pl_Motor_HallToggleTimeCalcClbk_t pHallToggleTimeCalcClbk);
-void Pl_TIM_HallToggleTimeCalc_Enable(void);
-void Pl_TIM_HallToggleTimeCalc_Disable(void);
-void Pl_TIM_HallToggleTimeCalc_ResetCnt(void);
-u32 Pl_TIM_HallToggleTimeCalc_GetCnt(void);
-
-void Pl_TIM_SpeedControl_Init(Pl_TIM_Motor_SpeedControlClbk_t pSpeedControlClbk);
-
-void Pl_DMA_AdcHighFreq_Enable(void);
-void Pl_ADC_StartHighFreqConv(void);
-
-void Pl_LedDebug_Init(void);
+void Pl_Led_Init(void);
 void Pl_Led_Set(void);
 void Pl_Led_Reset(void);
 void Pl_Led_Toggle(void);
-
-void Pl_Hall1_CallIrq(void);
 
 #endif /* __PLATFORM_H */
