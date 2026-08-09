b374984aebb3641723be8e9fa885e6efdf16742b
Dmitrii Leonov <dleonov@whoosh.bike>
2026-08-09 18:48:56 +0300

    on rtos overflow hook & fix hse clk

--- app/conf/FreeRTOSConfig.h
+++ app/conf/FreeRTOSConfig.h
@@ -385,7 +385,7 @@ PRIORITY THAN THIS! (higher priorities are lower numeric values. */
   * configCHECK_FOR_STACK_OVERFLOW is set to 1. See
   * https://www.freertos.org/Stacks-and-stack-overflow-checking.html  Defaults to
   * 0 if left undefined. */
-// #define configCHECK_FOR_STACK_OVERFLOW 2
+#define configCHECK_FOR_STACK_OVERFLOW 2
 
 /******************************************************************************/
 /* Run time and task stats gathering related definitions. *********************/
